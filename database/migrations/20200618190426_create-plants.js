7acc679105079781daa5b561e810ea3dcb0bcce9
Veto Ramirez <vetorobert3@gmail.com>
2020-06-18 22:55:22 -0700

    Commiting to push to build retro..

--- database/migrations/20200618190426_create-plants.js
+++ database/migrations/20200618190426_create-plants.js
@@ -1,7 +1,19 @@
 
 exports.up = function(knex) {
   return knex.schema
-    
+    .createTable('users', tbl => {
+      tbl.increments('id');
+      tbl
+        .string('username', 128)
+        .notNullable()
+        .unique();
+      tbl
+        .string('password', 255)
+        .notNullable();
+      tbl
+        .integer('phone number', 10)
+        .notNullable();
+    })
 };
 
 exports.down = function(knex) {
