f673f0650528feecfd8d6f88f8297e1d6a698ec7
Veto Ramirez <vetorobert3@gmail.com>
2020-06-22 20:54:57 -0700

    added a .env file

---
+++ plants/plants-router.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const router = express.Router();
+// const Plants = require('./plants-model.js');
+// const restricted = require('../auth/restricted-middleware.js');
+
+// // Gets
+// router.get('/', restricted, (req, res) => {
+
+// })
